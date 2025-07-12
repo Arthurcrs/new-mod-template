aef923653f876a46869e9b16d86655f7d7bbb7ad
Mahghuuuls <arthcrs@gmail.com>
2025-07-12 18:32:12 -0300

    Moved config to its own package

--- src/main/java/com/mahghuuuls/examplemod/config/ModConfig.java
+++ src/main/java/com/mahghuuuls/examplemod/config/ModConfig.java
@@ -1,14 +1,13 @@
-package com.mahghuuuls.examplemod.util;
+package com.mahghuuuls.examplemod.config;
+
+import com.mahghuuuls.examplemod.util.ModReference;
 
 import net.minecraftforge.common.config.Config;
 
 @Config(modid = ModReference.MOD_ID)
-public final class ModConfig {
+public class ModConfig {
 
 //	@Config.Comment("Example config")
 //	public static boolean blockMelee = true;
-
-	private ModConfig() {
-	}
 
 }
