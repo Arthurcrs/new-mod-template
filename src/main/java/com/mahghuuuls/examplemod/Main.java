bc26baefd45631cc241b243ea61d77e5bdab2468
Mahghuuuls <arthcrs@gmail.com>
2025-07-12 18:27:27 -0300

    Events should be subscribed in an EventHandler class, created the base for that, Main class now only has the initialization methods

--- src/main/java/com/mahghuuuls/examplemod/Main.java
+++ src/main/java/com/mahghuuuls/examplemod/Main.java
@@ -4,6 +4,8 @@ import com.mahghuuuls.examplemod.util.ModReference;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModReference.MOD_ID, name = ModReference.NAME, version = ModReference.VERSION)
@@ -11,6 +13,14 @@ public class Main {
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void preInit(FMLInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void preInit(FMLPostInitializationEvent event) {
 	}
 
 }
---
+++ src/main/java/com/mahghuuuls/examplemod/event/EventHandler.java
@@ -0,0 +1,10 @@
+package com.mahghuuuls.examplemod.event;
+
+import com.mahghuuuls.examplemod.util.ModReference;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ModReference.MOD_ID)
+public class EventHandler {
+
+}
