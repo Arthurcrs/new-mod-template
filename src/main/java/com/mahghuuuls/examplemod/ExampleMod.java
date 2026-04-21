33d5ba942824824632f96f4b702849e27641f153
Arthur Carlos <arthcrs@gmail.com>
2026-04-21 19:01:41 -0300

    initialize core proxy pattern and build pipeline

--- src/main/java/com/mahghuuuls/examplemod/ExampleMod.java
+++ src/main/java/com/mahghuuuls/examplemod/ExampleMod.java
@@ -1,31 +1,35 @@
 package com.mahghuuuls.examplemod;
 
+import com.mahghuuuls.examplemod.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ExampleMod.MOD_ID, name = ExampleMod.NAME, version = ExampleMod.VERSION, dependencies = ExampleMod.DEPENDENCIES)
 public class ExampleMod {
-	public static final String MOD_ID = "examplemod";
-	public static final String NAME = "Example Mod";
-	public static final String VERSION = "1.0.0";
-	public static final String DEPENDENCIES = "";
+    public static final String MOD_ID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0.0";
+    public static final String DEPENDENCIES = "";
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
+    @SidedProxy(clientSide = "com.mahghuuuls.examplemod.proxy.ClientProxy", serverSide = "com.mahghuuuls.examplemod.proxy.CommonProxy")
+    public static CommonProxy proxy;
 
-	}
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(); // On a client, this executes ClientProxy#preInit. On a server, CommonProxy#preInit.
+    }
 
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(); // On a client, this executes ClientProxy#preInit. On a server, CommonProxy#preInit.
+    }
 
-	}
-
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-
-	}
-
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(); // On a client, this executes ClientProxy#preInit. On a server, CommonProxy#preInit.
+    }
 }
--- src/main/java/com/mahghuuuls/examplemod/config/ModConfig.java
+++ src/main/java/com/mahghuuuls/examplemod/config/ModConfig.java
@@ -1,13 +1,20 @@
 package com.mahghuuuls.examplemod.config;
 
 import com.mahghuuuls.examplemod.ExampleMod;
-
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = ExampleMod.MOD_ID)
+@Mod.EventBusSubscriber(modid = ExampleMod.MOD_ID)
 public class ModConfig {
-
-//	@Config.Comment("Example config")
-//	public static boolean blockMelee = true;
-
+    @SubscribeEvent // This method fires when the user clicks "Done" in the in-game Mod Options menu
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(ExampleMod.MOD_ID)) {
+            // Forces Forge to read the updated values from the GUI and write them to your variables and the .cfg file
+            ConfigManager.sync(ExampleMod.MOD_ID, Config.Type.INSTANCE);
+        }
+    }
 }
